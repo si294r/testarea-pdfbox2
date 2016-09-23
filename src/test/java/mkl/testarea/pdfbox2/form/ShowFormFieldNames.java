a946c1d69e02f73645ddad00fc06752aeed563eb
mkl <mkl@wir-sind-cool.org>
2016-09-23 10:59:24 +0200

    Enhanced test ShowFormFieldNames focusing on the stackoverflow question
    How can the internal labels of the editable fields in an acroform .pdf
    be found and listed?
    http://stackoverflow.com/questions/39574021/how-can-the-internal-labels-of-the-editable-fields-in-an-acroform-pdf-be-found
    
    Alternative field name collecting method getFormFieldNamesFancy using a
    bit more fancy streaming methods.

--- src/test/java/mkl/testarea/pdfbox2/form/ShowFormFieldNames.java
+++ src/test/java/mkl/testarea/pdfbox2/form/ShowFormFieldNames.java
@@ -5,11 +5,14 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDTerminalField;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -39,6 +42,9 @@ public class ShowFormFieldNames
             {
                 System.out.printf("* '%s'\n", name);
             }
+
+            List<String> fieldNamesFancy = getFormFieldNamesFancy(pdDocument);
+            Assert.assertEquals(fieldNames, fieldNamesFancy);
         }
         
     }
@@ -66,5 +72,25 @@ public class ShowFormFieldNames
             }
         }
         return result;
+    }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/39574021/how-can-the-internal-labels-of-the-editable-fields-in-an-acroform-pdf-be-found">
+     * How can the internal labels of the editable fields in an acroform .pdf be found and listed?
+     * </a>
+     * <p>
+     * This method retrieves the form field names from the given {@link PDDocument}
+     * using a bit more fancy streaming methods. 
+     * </p>
+     */
+    List<String> getFormFieldNamesFancy(PDDocument pdDocument)
+    {
+        PDAcroForm pdAcroForm = pdDocument.getDocumentCatalog().getAcroForm();
+        if (pdAcroForm == null)
+            return Collections.emptyList();
+
+        return StreamSupport.stream(pdAcroForm.getFieldTree().spliterator(), false)
+                            .map(field -> field.getFullyQualifiedName())
+                            .collect(Collectors.toList());
     }
 }
