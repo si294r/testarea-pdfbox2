d47d7941a1d6e8a2c2602f4f7038fe8abeaad2c0
mkl <mkl@wir-sind-cool.org>
2016-09-26 14:34:39 +0200

    New test SetCropBox focusing on the stackoverflow question
    PDFBox 2.0.3 Set cropBox using TextPosition coordinates
    http://stackoverflow.com/questions/39689004/pdfbox-2-0-3-set-cropbox-using-textposition-coordinates
    ENG-US_NMATSCJ-1.103-0330.pdf
    http://downloadcenter.samsung.com/content/UM/201504/20150407095631744/ENG-US_NMATSCJ-1.103-0330.pdf
    
    This test shows how to set the crop box on page twelve.

---
+++ src/test/java/mkl/testarea/pdfbox2/meta/SetCropBox.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package mkl.testarea.pdfbox2.meta;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author mklink
+ *
+ */
+public class SetCropBox
+{
+    final static File RESULT_FOLDER = new File("target/test-outputs", "meta");
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+        RESULT_FOLDER.mkdirs();
+    }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/39689004/pdfbox-2-0-3-set-cropbox-using-textposition-coordinates">
+     * PDFBox 2.0.3 Set cropBox using TextPosition coordinates
+     * </a>
+     * <br/>
+     * <a href="http://downloadcenter.samsung.com/content/UM/201504/20150407095631744/ENG-US_NMATSCJ-1.103-0330.pdf">
+     * ENG-US_NMATSCJ-1.103-0330.pdf
+     * </a>
+     * <p>
+     * This test shows how to set the crop box on page twelve.
+     * </p>
+     */
+    @Test
+    public void testSetCropBoxENG_US_NMATSCJ_1_103_0330() throws IOException
+    {
+        try (   InputStream resource = getClass().getResourceAsStream("ENG-US_NMATSCJ-1.103-0330.pdf"))
+        {
+            PDDocument pdDocument = PDDocument.load(resource);
+            PDPage page = pdDocument.getPage(12-1);
+            page.setCropBox(new PDRectangle(40f, 680f, 510f, 100f));
+            pdDocument.save(new File(RESULT_FOLDER, "ENG-US_NMATSCJ-1.103-0330-page12cropped.pdf"));
+        }
+    }
+
+}
