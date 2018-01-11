fb05dc116e06f1c464f60902280dbe5ca31357ae
Michael Klink <Michael.Klink@exceet.de>
2018-01-11 09:36:42 +0100

    New CreateImageButton test focusing on the github question
    How to add image to pdf
    https://github.com/mkl-public/testarea-pdfbox2/issues/1
    
    This test shows how to change the appearance of a button to an image.

--- src/test/java/mkl/testarea/pdfbox2/form/CreateImageButton.java
+++ src/test/java/mkl/testarea/pdfbox2/form/CreateImageButton.java
@@ -19,6 +19,7 @@ import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationWidget;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceDictionary;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceStream;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDButton;
 import org.apache.pdfbox.pdmodel.interactive.form.PDPushButton;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -91,6 +92,52 @@ public class CreateImageButton
             acroForm.getFields().add(pdPushButton);
 
             document.save(new File(RESULT_FOLDER, "imageButton.pdf"));
+        }
+    }
+
+    /**
+     * <a href="https://github.com/mkl-public/testarea-pdfbox2/issues/1">
+     * How to add image to pdf
+     * </a>
+     * <p>
+     * This test shows how to change the appearance of a button to an image.
+     * </p>
+     */
+    @Test
+    public void testUpdateSimpleImageButton() throws IOException
+    {
+        try (   InputStream resource = getClass().getResourceAsStream("/mkl/testarea/pdfbox2/content/Willi-1.jpg");
+                InputStream sourceDoc = getClass().getResourceAsStream("imageButton.pdf");
+                PDDocument document = PDDocument.load(sourceDoc)) {
+            BufferedImage bufferedImage = ImageIO.read(resource);
+            PDImageXObject pdImageXObject = LosslessFactory.createFromImage(document, bufferedImage);
+            float width = 10 * pdImageXObject.getWidth();
+            float height = 10 * pdImageXObject.getHeight();
+
+            PDAppearanceStream pdAppearanceStream = new PDAppearanceStream(document);
+            pdAppearanceStream.setResources(new PDResources());
+            try (PDPageContentStream pdPageContentStream = new PDPageContentStream(document, pdAppearanceStream)) {
+                pdPageContentStream.drawImage(pdImageXObject, 0, 0, width, height);
+            }
+            pdAppearanceStream.setBBox(new PDRectangle(width, height));
+
+            PDAcroForm acroForm = document.getDocumentCatalog().getAcroForm();
+            PDButton button  = (PDPushButton)acroForm.getField("ImageButton");
+
+            List<PDAnnotationWidget> widgets = button.getWidgets();
+            for (PDAnnotationWidget pdAnnotationWidget : widgets) {
+
+                PDAppearanceDictionary pdAppearanceDictionary = pdAnnotationWidget.getAppearance();
+                if (pdAppearanceDictionary == null) {
+                    pdAppearanceDictionary = new PDAppearanceDictionary();
+                    pdAnnotationWidget.setAppearance(pdAppearanceDictionary);
+                }
+
+                pdAppearanceDictionary.setNormalAppearance(pdAppearanceStream);
+            }
+            button.setReadOnly(true);
+
+            document.save(new File(RESULT_FOLDER, "imageButtonUpdated.pdf"));
         }
     }
 }
