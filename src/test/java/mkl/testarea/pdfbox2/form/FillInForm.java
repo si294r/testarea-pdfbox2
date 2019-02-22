0c1bf2f670effe05ea6e1f898b21c4bc260057a2
mkl <mkl@wir-sind-cool.org>
2019-02-22 13:13:03 +0100

    New FillInForm test focusing on the stack overflow question
    Why is PDFBox overwriting multiple Fields, even when they don't match
    the fullyQualifiedName? (Kotlin Android)
    https://stackoverflow.com/questions/54820224/why-is-pdfbox-overwriting-multiple-fields-even-when-they-dont-match-the-fullyq
    csOnePage.pdf
    http://www.kylevp.com/csOnePage.pdf
    
    The problem is due to some fields of the form sharing empty appearance
    XObjects and PDFBox assuming in case of existing appearance XObjects
    that it can simply update this existing appearance instead of having to
    create a new one from scratch.
    
    A work-around is to remove existing appearances from a field before
    setting its value.

--- src/test/java/mkl/testarea/pdfbox2/form/FillInForm.java
+++ src/test/java/mkl/testarea/pdfbox2/form/FillInForm.java
@@ -3,6 +3,7 @@ package mkl.testarea.pdfbox2.form;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.pdfbox.cos.COSDictionary;
@@ -179,6 +180,51 @@ public class FillInForm
 
             document.save(new File(RESULT_FOLDER, "testform-filled.pdf"));
             document.close();
+        }
+    }
+
+    /**
+     * <a href="https://stackoverflow.com/questions/54820224/why-is-pdfbox-overwriting-multiple-fields-even-when-they-dont-match-the-fullyq">
+     * Why is PDFBox overwriting multiple Fields, even when they don't match the fullyQualifiedName? (Kotlin Android)
+     * </a>
+     * <br/>
+     * <a href="http://www.kylevp.com/csOnePage.pdf">
+     * csOnePage.pdf
+     * </a>
+     * <p>
+     * The problem is due to some fields of the form sharing empty
+     * appearance XObjects and PDFBox assuming in case of existing
+     * appearance XObjects that it can simply update this existing
+     * appearance instead of having to create a new one from scratch.
+     * </p>
+     * <p>
+     * A work-around is to remove existing appearances from a field
+     * before setting its value, see below.
+     * </p>
+     */
+    @Test
+    public void testLikeKyle() throws IOException {
+        try (   InputStream originalStream = getClass().getResourceAsStream("csOnePage.pdf") )
+        {
+            PDDocument doc = PDDocument.load(originalStream);
+            PDAcroForm acroForm = doc.getDocumentCatalog().getAcroForm();
+
+            List<String> skillList = Arrays.asList("Athletics","Acrobatics","Sleight of Hand", "Stealth","Acrana", "History","Investigation","Nature", "Religion", "Animal Handling", "Insight", "Medicine", "Perception", "Survival", "Deception", "Intimidation", "Performance", "Persuasion");
+
+            int temp = 0;
+            for (String skill : skillList) {
+                PDField field = acroForm.getField(skill);
+                temp += 1;
+                if (field == null) {
+                    System.err.printf("(%d) field '%s' is null.\n", temp, skill);
+                } else {
+                    field.getCOSObject().removeItem(COSName.AP);
+                    field.setValue(String.valueOf(temp));
+                }
+            }
+
+            doc.save(new File(RESULT_FOLDER, "csOnePage-filled.pdf"));
+            doc.close();
         }
     }
 }
